26f4bb4a6970718b9cc7b71e169147eb76c8a445
ra1n <64748227+spacepilotAV@users.noreply.github.com>
2021-04-02 13:21:29 -0400

    remove unnecessary config variables

--- Athenus/config.h
+++ Athenus/config.h
@@ -19,13 +19,4 @@
 #define TRUE    1
 #define FALSE   0
 
-#define DISPLAY_CODE_EXECUTION_NOTICE   TRUE
-#define DISPLAY_DONE_NOTICE             TRUE
-#define RUN_KERNEL_EXPLOIT              FALSE
-#define RUN_NOTIF_TEST                  FALSE
-
-#define CODE_EXEC_NOTICE                "achieved code execution"
-#define DONE_NOTICE                     "achieved code execution"
-
-
 #endif /* config_h */
