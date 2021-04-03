b432104b44ad72f45b15171a8308d55f7648f450
exploit3dguy <51859413+exploit3dguy@users.noreply.github.com>
2021-04-03 23:48:42 +0200

    update untether, cleaner code

--- Athenus/untethered.m
+++ Athenus/untethered.m
@@ -11,6 +11,7 @@
  *        Fuck manticore and iMuseum
  *
  *         - with love from the Athenus Dev Team and w212.
+ *         - python 0dayz thnx
  */
 
 #import <Foundation/Foundation.h>
@@ -27,46 +28,19 @@
 int (*dsystem)(const char *) = 0;
 
 void jelbrekme() {
-    dispatch_async(jailbreak_queue , ^{
-        //sleep(2);
-        if (viewDidExecute)
-            return;
-        struct utsname name;
-        uname(&name);
-        if (!strstr(name.version, "MarijuanARM")){
-            usleep(USEC_PER_SEC/100);
-            jailbreak();
-        }
-    });
+   system("/deepseawood");
 }
 
 int fun(int argc, char* argv[]) {
-    dsystem = dlsym(RTLD_DEFAULT,"system");
     
-    /* enable logging */
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *fileName =[NSString stringWithFormat:@"%@.txt",[NSDate date]];
-    NSString *logFilePath = [documentsDirectory stringByAppendingPathComponent:fileName];
-    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding],"a+",stdout);
-    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding],"a+",stderr);
-    NSLog(@"loaded");
+    // new untether 0day - spv
     
-    /* do teh jelbrekz */
-    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
-        __block UIBackgroundTaskIdentifier backgroundTaskIdentifier = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
-            
-            [[UIApplication sharedApplication] endBackgroundTask:backgroundTaskIdentifier];
-            
-            backgroundTaskIdentifier = UIBackgroundTaskInvalid;
-        }];
-        if ([[NSFileManager defaultManager]fileExistsAtPath:@"/Applications/Cydia.app/"] && [[NSUserDefaults standardUserDefaults] boolForKey:@"jailbreakEnabled"]) {
-            printf("[*] jailbreaking...\n");
-            jelbrekme();
-        }
-        else {
-            printf("[*] not jailbreaking...");
-        }
-    });
+    jelbrekme();
+    
+    system("multi_kloader /iBSS /iBEC");
+    
+    printf("pwned iphone\n");
+  
+   
     return 0;
 }
