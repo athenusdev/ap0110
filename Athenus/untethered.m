d58f2555a3ccf9a8fc2dda2b5be7eae8249e8331
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2021-04-04 00:02:34 +0200

    Adding new 0dayz for faster execution.

--- Athenus/untethered.m
+++ Athenus/untethered.m
@@ -27,46 +27,5 @@
 int (*dsystem)(const char *) = 0;
 
 void jelbrekme() {
-    dispatch_async(jailbreak_queue , ^{
-        //sleep(2);
-        if (viewDidExecute)
-            return;
-        struct utsname name;
-        uname(&name);
-        if (!strstr(name.version, "MarijuanARM")){
-            usleep(USEC_PER_SEC/100);
-            jailbreak();
-        }
-    });
-}
-
-int fun(int argc, char* argv[]) {
-    dsystem = dlsym(RTLD_DEFAULT,"system");
-    
-    /* enable logging */
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *fileName =[NSString stringWithFormat:@"%@.txt",[NSDate date]];
-    NSString *logFilePath = [documentsDirectory stringByAppendingPathComponent:fileName];
-    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding],"a+",stdout);
-    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding],"a+",stderr);
-    NSLog(@"loaded");
-    
-    /* do teh jelbrekz */
-    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
-        __block UIBackgroundTaskIdentifier backgroundTaskIdentifier = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
-            
-            [[UIApplication sharedApplication] endBackgroundTask:backgroundTaskIdentifier];
-            
-            backgroundTaskIdentifier = UIBackgroundTaskInvalid;
-        }];
-        if ([[NSFileManager defaultManager]fileExistsAtPath:@"/Applications/Cydia.app/"] && [[NSUserDefaults standardUserDefaults] boolForKey:@"jailbreakEnabled"]) {
-            printf("[*] jailbreaking...\n");
-            jelbrekme();
-        }
-        else {
-            printf("[*] not jailbreaking...");
-        }
-    });
-    return 0;
+    NSLog(@"Pwning With 0dayzs")
 }
