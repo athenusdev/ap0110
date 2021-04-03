ccbc7daf66b6b4965231a216865fc792bb8f5206
ra1n <64748227+spacepilotAV@users.noreply.github.com>
2021-04-02 21:38:29 -0400

    fix supported devices & the background of the app

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm Payload.ipa
 xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -sdk iphoneos
 mkdir Payload
 mv build/Release-iphoneos/ap0110.app Payload/
+cp Athenus/Ap0110-1024.png Payload/ap0110.app/
 zip -r Payload.ipa Payload
 rm -rf Payload
 rm -rf build
