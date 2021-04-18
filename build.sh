3d3b1ed9146f6b8ab1a8b6d7e9880974cdc55a99
ra1n <64748227+spacepilotAV@users.noreply.github.com>
2021-04-18 12:45:56 -0400

    make our life and anyone building ap0110's life *slightly* easier if making a release on GitHub

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ xcodebuild build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -sdk iphoneos
 mkdir Payload
 mv build/Release-iphoneos/ap0110.app Payload/
 cp Athenus/Ap0110-1024.png Payload/ap0110.app/
-zip -r Payload.ipa Payload
+zip -r ap0110.ipa Payload
 rm -rf Payload
 rm -rf build
 xcodebuild clean
